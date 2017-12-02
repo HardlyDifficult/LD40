119df9f267c741a8102bbf0e439bce0d812e302c
HardlyDifficult <HardlyDifficultLLC@gmail.com>
2017-12-01 17:37:06 -0800

    Configure for WebGL.
    Add AutoSave

---
+++ Assets/Scripts/Editor/AutoSave.cs
@@ -0,0 +1,35 @@
+﻿using UnityEditor;
+using UnityEditor.SceneManagement;
+
+/// <summary>
+/// An editor script which automatically saves every time you
+/// hit play.
+/// </summary>
+[InitializeOnLoad]
+public class AutoSave
+{
+  #region Init
+  /// <summary>
+  /// Called before the game starts c/o InitializeOnLoad.
+  /// </summary>
+  static AutoSave()
+  {
+    EditorApplication.playmodeStateChanged
+      += OnPlaymodeStateChanged;
+  }
+  #endregion
+
+  #region Events
+  /// <summary>
+  /// Called on enter/exit play mode and pause.
+  /// </summary>
+  static void OnPlaymodeStateChanged()
+  {
+    // Skip saving if in play mode, e.g. during pause
+    if (EditorApplication.isPlaying == false)
+    {
+      EditorSceneManager.SaveOpenScenes();
+    }
+  }
+  #endregion
+}
