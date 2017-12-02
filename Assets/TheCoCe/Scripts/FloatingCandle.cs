735bba83e056162a6d1442a303b87df87edbf305
Serafin Kaiser <serafinkaiser@yahoo.de>
2017-12-02 22:30:58 +0100

    Floating candles lighting

--- Assets/TheCoCe/Scripts/FloatingCandle.cs
+++ Assets/TheCoCe/Scripts/FloatingCandle.cs
@@ -2,21 +2,28 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FloatingCandle : MonoBehaviour {
+public class FloatingCandle : MonoBehaviour
+{
 
     private Vector3 position;
     private float randomSpeed;
     [SerializeField]
     private float magnitude = 0.5f;
-	// Use this for initialization
-	void Awake () {
+    [SerializeField]
+    private Light light;
+    // Use this for initialization
+    void Awake()
+    {
         position = transform.position;
         randomSpeed = Random.Range(0.5f, 2f) - 1f;
+        light = GetComponentInChildren<Light>();
     }
-	
-	// Update is called once per frame
-	void Update () {
+
+    // Update is called once per frame
+    void Update()
+    {
         float Offset = Mathf.Sin(Time.time);
         transform.position = position + new Vector3(Mathf.Sin(Time.time + randomSpeed) * randomSpeed * magnitude, Offset * randomSpeed * magnitude, 0f);
-	}
+        light.intensity = 1 + (Mathf.Sin(Time.time * 4f * randomSpeed) * 0.5f);
+    }
 }
