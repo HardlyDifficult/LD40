f3bf54dc76ff9b6f9f6d2c0318cdb71796be3f22
HardlyDifficult <HardlyDifficultLLC@gmail.com>
2017-12-02 11:13:39 -0800

    Added roof.
    Photon join room.

---
+++ Assets/Scripts/Network/NetworkController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NetworkController : MonoBehaviour
+{
+  [SerializeField]
+  PhotonView ballPrefab;
+
+  [SerializeField]
+  float zPositionPlayer1;
+
+  [SerializeField]
+  float zPositionPlayer2;
+
+  protected void Awake()
+  {
+    PhotonNetwork.ConnectUsingSettings("0.1");
+  }
+
+  protected void OnJoinedLobby()
+  {
+    PhotonNetwork.JoinRandomRoom();
+  }
+
+  protected void OnPhotonRandomJoinFailed()
+  {
+    RoomOptions roomOptions = new RoomOptions();
+    roomOptions.MaxPlayers = 2;
+    PhotonNetwork.CreateRoom(null, roomOptions, TypedLobby.Default);
+  }
+
+  protected void OnJoinedRoom()
+  {
+    PhotonPlayer[] playerList = PhotonNetwork.playerList;
+    if (playerList.Length == 1)
+    {
+      ballPrefab.RequestOwnership();
+    }
+    else
+    {
+      ball2.RequestOwnership();
+    }
+  }
+}
