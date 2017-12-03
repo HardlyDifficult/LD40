2270487f2d4af85bfff1aad9397f0b85c5373a7d
Benjamin <bkonsem@gmail.com>
2017-12-03 22:42:35 +0100

    Menu with a new font

--- Assets/Scripts/UI/ButtonPlayNow.cs
+++ Assets/Scripts/UI/ButtonPlayNow.cs
@@ -1,13 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
-public class ButtonPlayNow : MonoBehaviour
+public class ButtonPlayNow : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
-  public void Play()
-  {
-    SceneManager.LoadScene("PrototypeScene");
-  }
+    private Text buttonText;
+
+    public void Play()
+    {
+        SceneManager.LoadScene("PrototypeScene");
+    }
+
+    protected void Awake()
+    {
+        this.buttonText = this.GetComponentInChildren<Text>();
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        this.buttonText.color = Color.white;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        this.buttonText.color = Color.gray;
+    }
 }
