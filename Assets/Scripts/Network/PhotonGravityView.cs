31e94d6e0354ac0ae5a7c3387e61702944e72275
HardlyDifficult <HardlyDifficultLLC@gmail.com>
2017-12-02 16:50:15 -0800

    Sync gravity.

---
+++ Assets/Scripts/Network/PhotonGravityView.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PhotonView))]
+[RequireComponent(typeof(Rigidbody))]
+public class PhotonGravityView : MonoBehaviour
+{
+  Rigidbody m_Body;
+
+  void Awake()
+  {
+    this.m_Body = GetComponent<Rigidbody>();
+  }
+
+  public void OnPhotonSerializeView(
+    PhotonStream stream,
+    PhotonMessageInfo info)
+  {
+    if (stream.isWriting == true)
+    {
+      stream.SendNext(this.m_Body.useGravity);
+    }
+    else
+    {
+      this.m_Body.useGravity = (bool)stream.ReceiveNext();
+    }
+  }
+}
