d1386aa6b16bf3be258076c999db7438b0e0ef54
HardlyDifficult <HardlyDifficultLLC@gmail.com>
2017-12-03 13:39:01 -0800

    Floor collider.

--- Assets/Scripts/DestroyBallOnContact.cs
+++ Assets/Scripts/DestroyBallOnContact.cs
@@ -1,15 +1,35 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class DestroyBallOnContact : MonoBehaviour
 {
+  const int playerLayer = 8;
 
+  [SerializeField]
+  float howLongTillDestroy = 2;
 
-  void Start()
+  protected void OnCollisionEnter(
+    Collider collision)
   {
+    if(collision.gameObject.layer != playerLayer)
+    {
+      return;
+    }
 
+    StartCoroutine(DestroyBall());
   }
 
+  protected void OnCollisionExit(
+    Collider collision)
+  {
+    StopAllCoroutines();
+  }
 
+  IEnumerator DestroyBall()
+  {
+    yield return new WaitForSeconds(howLongTillDestroy);
+    Destroy(gameObject);
+  }
 }
