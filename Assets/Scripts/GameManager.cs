c485d10ba3d1e6bb0a8fc21f068f0880333c08c4
HardlyDifficult <HardlyDifficultLLC@gmail.com>
2017-12-02 16:22:24 -0800

    WIP

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+  public static GameManager instance;
+
+  protected void Awake()
+  {
+    if(instance != null)
+    {
+      Destroy(gameObject);
+    }
+
+    DontDestroyOnLoad(gameObject);
+    instance = this;
+  }
+}
