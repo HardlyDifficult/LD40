98ed4c7d0b168fba202285bd82e684de69205565
Benjamin <bkonsem@gmail.com>
2017-12-05 01:27:12 +0100

    Mainmenu & button colors & stuff

---
+++ Assets/btastical/VolumeSlider.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VolumeSlider : MonoBehaviour
+{
+    private Slider volumeSlider;
+
+    // Use this for initialization
+    void Start ()
+	{
+	    this.volumeSlider = this.GetComponent<Slider>();
+	    this.volumeSlider.value = AudioListener.volume;
+	}
+
+    // Update is called once per frame
+    void Update ()
+	{
+	    AudioListener.volume = this.volumeSlider.value;
+	}
+}
